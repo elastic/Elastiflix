13485ced823f6f7dd05c81c85dbb8d020f326af2
Lionel Palacin <lionel.palacin@gmail.com>
2022-10-31 12:25:52 +0100

    simplify deployment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,9 +12,9 @@ import { SearchProvider } from "@elastic/react-search-ui";
 function SearchBar() {
 
     const connector = new AppSearchAPIConnector({
-        searchKey: window._env_.AS_SEARCH_API_KEY,
-        engineName: window._env_.ENGINE_NAME,
-        endpointBase: window._env_.AS_BASE_URL,
+        searchKey: process.env.REACT_APP_AS_SEARCH_API_KEY,
+        engineName: process.env.REACT_APP_ENGINE_NAME,
+        endpointBase: process.env.REACT_APP_AS_BASE_URL,
         cacheResponses: false
     });
 
