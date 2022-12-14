7dcc274450c6f2f6adffde11e32e278308946d9b
Lionel Palacin <lionel.palacin@gmail.com>
2022-12-14 13:26:05 +0000

    change environment

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 Elastiflix is a fictitious video streaming service company that provides a web-based user interface leveraging data from [The Internet Movie Database](https://tmdb.org/). Elastiflix provides a mechanism to query data from an indexed copy of TMDB data that sits in Elastic Cloud, allowing for high-performant, flexible management and querying of that data in order to provide an exceptional search experience.
 
-## Pre-reqs
+## Pre-requisites
 
 - [Elastic Cloud](https://cloud.elastic.co/) Deployment created, new accounts can sign-up for a limitless 14-day free trial to POC building a solution like this
-- Docker Desktop Installed
 
 ### Setup Elastic Cloud
 
@@ -28,20 +27,23 @@ Once the cluster is ready, you can navigate to it at the link provided. This wil
 <img src="static/loader/get_as_base_url.gif" height="500">
 </p>
 
-### Running the sample
+### Load data
 
-This repository consists of 2 modules, that can be run as docker containers to show an End to End experience of ingesting, managing and searching with Elastic Cloud.
+#### The data files
 
-The modules are
+There are a handful of files that can be updated to simulate data. They can also be used as-is with great results
 
-- [Data Ingestion](/src/data-loader)
-- [Search Experiences](/src/streaming-ui)
+- tmdb-data/cast_popular: JSON file from TMDB that shows popular movies
+- tmdb-data/movies_2000-2021_w_details.json: JSON file representing over 8k movies with additional details
+- analytics-data/query_no_results.txt: File containing queries that will return no results
+- analytics-data/terms.txt: This is a generated file that contains random search terms.
 
+#### Run the scripts
 
-### What's next?
+There are 2 scripts that run when you create the container: one that generates sample term data and another that performs the simulation against Elastic Cloud
 
-These 2 parts are just the start to building great search solutions, as Elastic also provides pre-built tools to help accelerate the building out of a top-notch search experience. You can find out more information about this at the [Elastic Enterprise Search](https://www.elastic.co/enterprise-search) section of the Elatic website.
-
+- generate-analytics-input.py: File that reads from TMDB movie files and randomly create list of search terms.
+- generate-analytics.py: Aggregates all data files to perform different searches
 
 
 # Elastiflix Website
@@ -49,7 +51,6 @@ These 2 parts are just the start to building great search solutions, as Elastic 
 React App preconfigured to query movies from Elastic Cloud deployment created in the [TMDB Loader Section](../data-loader/). The search experience is powered by [Search UI](https://github.com/elastic/search-ui), a free open source package written in React and Typescript to accelerate creating visual interfaces for search written and maintained by Elastic.
 
 ## Prerequisites
-
 
 - [Data Import step completed](../data-loader)
 - Docker Desktop Installed
@@ -61,16 +62,12 @@ React App preconfigured to query movies from Elastic Cloud deployment created in
 
     - ENGINE_NAME: LEAVE UNTOUCHED
 
-## Build the docker image
-
-```bash
-docker build . -t elastiflix-ui:latest
-docker run -d --env-file scripts/.env -p 5000:80 elastiflix-ui
-```
-
-if you get an error similar to `The engine "node" is incompatible with this module.`, try pulling the latest version of `docker pull node:12` or run the docker build command with the additional flag to disable caching: `docker build . --no-cache -t elastiflix-ui:latest`
-
 ## Run App
 
 Open browser to https://localhost:5000 to see the UI in action!
    <img src="../../static/ui/ui.png" height="700">
+
+### What's next?
+
+These 2 parts are just the start to building great search solutions, as Elastic also provides pre-built tools to help accelerate the building out of a top-notch search experience. You can find out more information about this at the [Elastic Enterprise Search](https://www.elastic.co/enterprise-search) section of the Elatic website.
+
--- data-loader/index-data.py
+++ data-loader/index-data.py
@@ -11,11 +11,11 @@
 import json
 import os
 import argparse
-import requests
-import random
+import gzip
 from elastic_enterprise_search import AppSearch
 from elasticsearch import Elasticsearch, helpers
-
+import time
+import random
 # index: Index the data from the JSON files into Elastic Cloud
 # Arguments:
 #   - moviesFile: Filepath containing the movies JSON data
@@ -33,77 +33,78 @@ def index(moviesFile):
       response = app_search.index_documents(engine_name=args.engine_name,documents=movies,request_timeout=60)
       movies = []
       count = 0
+      time.sleep(3)
       print(".", end='', flush=True)
 
 parser = argparse.ArgumentParser()
 #required args
-parser.add_argument('--data_folder', dest='data_folder', required=False)
-parser.add_argument('--config_folder', dest='config_folder', required=False)
-parser.add_argument('--es_user', dest='es_user', required=False, default='elastic')
-parser.add_argument('--es_password', dest='es_password', required=True)
+parser.add_argument('--data_folder', dest='data_folder', required=False, default='movies')
+parser.add_argument('--config_folder', dest='config_folder', required=False, default='config')
+parser.add_argument('--private_key', dest='pkey', required=False, default='config')
+# parser.add_argument('--es_user', dest='es_user', required=False, default='elastic')
+# parser.add_argument('--es_password', dest='es_password', required=True)
 parser.add_argument('--as_host', dest='as_host', required=True)
 parser.add_argument('--analytics_folder', dest='analytics_folder', required=False)
-parser.add_argument('--engine_name', dest='engine_name', required=True)
-parser.add_argument('--cloud_id', dest='cloud_id', required=True)
+parser.add_argument('--engine_name', dest='engine_name', required=False, default='movies')
+# parser.add_argument('--cloud_id', dest='cloud_id', required=True)
 args = parser.parse_args()
 
-print("Retrieve app search credentials")
-as_private_key = None
-responseRead = requests.get(args.as_host+'/api/as/v1/credentials/pkey', auth=(args.es_user, args.es_password))
+# print("Retrieve app search credentials")
+# as_private_key = None
+# responseRead = requests.get(args.as_host+'/api/as/v1/credentials/pkey', auth=(args.es_user, args.es_password))
 
-if responseRead.ok:
-    as_private_key = responseRead.json()['key']
-else:
-    print("Private key does not exist, creating it")
-    body = {'name': 'pkey', "type": "private", "read": True, "write": True, "access_all_engines": True}
-    responseWrite = requests.post(args.as_host+'/api/as/v1/credentials', json = body, auth=(args.es_user, args.es_password))
-    if responseWrite.ok:
-        as_private_key = responseWrite.json()['key']
-print("App search credentials found")
+# if responseRead.ok:
+#     as_private_key = responseRead.json()['key']
+# else:
+#     print("Private key does not exist, creating it")
+#     body = {'name': 'pkey', "type": "private", "read": True, "write": True, "access_all_engines": True}
+#     responseWrite = requests.post(args.as_host+'/api/as/v1/credentials', json = body, auth=(args.es_user, args.es_password))
+#     if responseWrite.ok:
+#         as_private_key = responseWrite.json()['key']
+
+# print("App search credentials found")
 app_search = AppSearch(
     args.as_host,
-    http_auth=as_private_key
+    http_auth=args.pkey
 )
 print("Create engine")
 app_search.create_engine(engine_name=args.engine_name, request_timeout=30)
 print("Engine created")
 
 print("Indexing data to App Search")
-for data_filename in os.listdir(args.data_folder):
-    if data_filename.endswith('.json'):
-        data_file = open(os.path.join(args.data_folder, data_filename), 'r')
-        index(data_file)
-
+moviesFile = gzip.open(os.path.join(args.data_folder, "movies.json.gz"), 'rt')
+index(moviesFile)
+time.sleep(60)
 print("Update engine schema")
 schemaConfig = open(os.path.join(args.config_folder, "schema.json") , "r")
 schemaJson = json.load(schemaConfig)
 resp = app_search.put_schema(
     engine_name=args.engine_name,
     schema=schemaJson,
-    request_timeout=30
+    request_timeout=60
 )
 
 print("Update engine relevancy")
 relevancyConfig = open(os.path.join(args.config_folder, "relevancy.json"), "r")
 relevancyJson = json.load(relevancyConfig)
-app_search.put_search_settings(engine_name=args.engine_name,body=relevancyJson,request_timeout=30)
+app_search.put_search_settings(engine_name=args.engine_name,body=relevancyJson,request_timeout=60)
 
 print("Upload synonyms set")
 synonymsConfig = open(os.path.join(args.config_folder, "synonyms.json"), "r")
 synonymsConfig = json.load(synonymsConfig)
 for synonym in synonymsConfig:
-    app_search.create_synonym_set(engine_name=args.engine_name,body={"synonyms": synonym},request_timeout=30)
+    app_search.create_synonym_set(engine_name=args.engine_name,body={"synonyms": synonym},request_timeout=60)
 
-print("Indexing data to Elasticsearch")
-es = Elasticsearch(
-    cloud_id=args.cloud_id,
-    http_auth=(args.es_user, args.es_password),
-    request_timeout=30
-)
+# print("Indexing data to Elasticsearch")
+# es = Elasticsearch(
+#     cloud_id=args.cloud_id,
+#     http_auth=(args.es_user, args.es_password),
+#     request_timeout=30
+# )
 
-moviesFile = open(os.path.join(args.data_folder, "movies_2000-2021_w_details.json"), "r")
-moviesJson = json.load(moviesFile)
-helpers.bulk(es, moviesJson, index='movies')
+# moviesFile = open(os.path.join(args.data_folder, "movies_2000-2021_w_details.json"), "r")
+# moviesJson = json.load(moviesFile)
+# helpers.bulk(es, moviesJson, index='movies')
 
 
 
@@ -113,7 +114,7 @@ terms_array = []
 for term in terms.readlines():
     terms_array.append(term)
 
-cast_popular_json = json.load(open(os.path.join(args.data_folder, "cast_popular.json"), "r"))
+cast_popular_json = json.load(open(os.path.join(args.analytics_folder, "cast_popular.json"), "r"))
 c_cast = 0
 while c_cast < 20:
     terms_array.append(cast_popular_json[c_cast]['name'].replace('.csv', '').lower())
