4cf4aa5335dd0606d3785e3ea475d16c4c28400f
Lionel Palacin <lionel.palacin@gmail.com>
2022-12-16 11:31:59 +0000

    remove webvitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import '@elastic/eui/dist/eui_theme_light.css';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
@@ -14,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
