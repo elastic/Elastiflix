7d0ee7d8088d7dee764f40dcdb4ebff8600aa8b0
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2024-12-11 13:32:03 -0500

    refactor: migrate to React 18's createRoot API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-import '@elastic/eui/dist/eui_theme_light.css';
 import './index.scss';
 import App from './App';
 
 
-ReactDOM.render(
-
-    <App />,
-  document.getElementById('root')
-);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
