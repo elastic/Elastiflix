14c2a81a84eb0ed19f3c93480ab6cf7a381ec85a
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2024-12-19 16:50:21 -0500

    fix import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; 
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import './index.scss';
 import App from './App';
