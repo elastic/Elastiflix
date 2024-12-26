8bfcc3e549c70af8ca7f0a64d40c4dd85f53da98
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2024-12-26 11:29:22 -0500

    Header with a search query and filters instead of hard coded content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,71 @@
-function Header() {
-    return (
-      <header className="banner">
-        <div className="banner__contents">
-          <h1 className="banner__title">Luca</h1>
-          <div className="banner__buttons">
-            <button className="banner__button">Play</button>
-            <button className="banner__button">My List</button>
-          </div>
-          <p className="banner__description">Luca and his best friend Alberto experience an unforgettable summer on the Italian Riviera. But all the fun is threatened by a deeply-held secret: they are sea monsters...</p>
-        </div>
-        <div className="banner--fadeBottom"></div>
-      </header>
-    );
-  }
+import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
+import { SearchProvider, WithSearch } from "@elastic/react-search-ui";
 
-  export default Header;
+function Header() {
+  const connector = new ElasticsearchAPIConnector({
+    host: process.env.REACT_APP_ES_BASE_URL,
+    index: process.env.REACT_APP_INDEX_NAME,
+    apiKey: process.env.REACT_APP_ES_SEARCH_API_KEY,
+  });
+
+  const config = {
+    initialState: { sortDirection: "desc", sortField: "popularity", resultsPerPage: 1 }, // Only fetch 1 result
+    searchQuery: {
+      filters: [
+        {
+          field: "id", 
+          values: ["568124"], // Match this field's value to the passed `id`
+          type: "all", 
+        }
+      ],
+      result_fields: {
+        title: { raw: {} },
+        poster_path: { raw: {} },
+        overview: { raw: {} },
+        release_date: { raw: {} },
+      },
+    },
+    apiConnector: connector,
+    alwaysSearchOnInitialLoad: true,
+    trackUrlState: false,
+  };
+
+  return (
+    <SearchProvider config={config}>
+      <WithSearch
+        mapContextToProps={({ results }) => ({ results })} // Map only the results
+      >
+        {({ results }) => {
+          if (!results || results.length === 0) return <div>Loading...</div>;
+
+          // Get the first result
+          const movie = results[0];
+          const poster = movie.poster_path.raw
+            ? `https://image.tmdb.org/t/p/original/${movie.poster_path.raw}`
+            : null;
+
+          return (
+            <header
+              className="banner"
+              style={{
+                backgroundImage: `url(${poster})`,
+              }}
+            >
+              <div className="banner__contents">
+                <h1 className="banner__title">{movie.title.raw}</h1>
+                <div className="banner__buttons">
+                  <button className="banner__button">Play</button>
+                  <button className="banner__button">My List</button>
+                </div>
+                <p className="banner__description">{movie.overview.raw}</p>
+              </div>
+              <div className="banner--fadeBottom"></div>
+            </header>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
+}
+
+export default Header;
