04be0ca5caed8da8d9475744620475724ffe6d92
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2025-05-21 08:42:43 -0500

    searchbar without autocomplete for instruqt

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import { EuiIcon } from '@elastic/eui';
 import { useHistory } from "react-router-dom";
 import connector from '../services/SearchConnector'; 
 
-const renderInput = ({ getAutocomplete, getInputProps }) => {
+const renderInput = ({ getInputProps }) => {
     return (
         <div className="search-box">
             <EuiIcon className="search-box__icon" type="search" />
@@ -13,7 +13,6 @@ const renderInput = ({ getAutocomplete, getInputProps }) => {
                     placeholder: "Search by title, cast name..."
                 })}
             />
-        {getAutocomplete()}
         </div>
     )
 }
@@ -25,15 +24,7 @@ function SearchBar() {
     const config = {
         apiConnector: connector, 
         trackUrlState: false,
-        alwaysSearchOnInitialLoad: false,
-        autocompleteQuery: {
-            results: {
-                resultsPerPage: 5,
-                result_fields: {
-                    title: { snippet: { size: 100, fallback: true }}
-                }
-            }
-        }
+        alwaysSearchOnInitialLoad: false
     };
 
     return (
@@ -46,16 +37,7 @@ function SearchBar() {
                     history.push("/search?q=" + searchTerm);
                     window.location.href = "/search?q=" + searchTerm;
                 }}
-                autocompleteMinimumCharacters={2}
-                autocompleteResults={{
-                    sectionTitle: "Results",
-                    titleField: "title",
-                }}
-                onSelectAutocomplete={(selection) => {
-                    if (selection.title) {
-                        window.location.href = "/search?q=" + selection.title.raw;
-                    }
-                }}
+                
             />
         </SearchProvider>
     );
