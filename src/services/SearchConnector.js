cec173130205c83f78c4c2832a108d48d2aed381
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2025-05-30 10:02:30 -0500

    update secrets

---
+++ src/services/SearchConnector.js
@@ -0,0 +1,16 @@
+import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
+
+const connectorOptions = {
+  host: process.env.REACT_APP_SEARCH_ES,
+  index: process.env.REACT_APP_SEARCH_INDEX,
+  ...(process.env.REACT_APP_SEARCH_API_KEY && { apiKey: process.env.REACT_APP_SEARCH_API_KEY }),
+  connectionOptions: {
+    headers: {
+      credentials: "include"
+    }
+  }
+};
+
+const connector = new ElasticsearchAPIConnector(connectorOptions);
+
+export default connector;
