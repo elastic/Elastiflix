e41d719c6ef3d7388d1138a76251fce0fb52cf32
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2025-06-13 08:37:38 -0500

    data loader script

--- data-loader/index-data.py
+++ data-loader/index-data.py
@@ -2,7 +2,7 @@ import json
 import os
 import argparse
 import gzip
-from elasticsearch import Elasticsearch, helpers
+from elasticsearch import Elasticsearch, helpers, NotFoundError
 from tqdm import tqdm
 
 def actions_generator(movies_json, index_name):
@@ -22,7 +22,9 @@ def main():
     parser.add_argument('--es_host', dest='es_host', required=True)
     parser.add_argument('--index_name', dest='index_name', required=False, default='movies')
     parser.add_argument('--recreate', dest='recreate', action=argparse.BooleanOptionalAction, required=False, default=False)
-    
+    parser.add_argument('--create_inference_endpoints', dest='create_inference_endpoints', action=argparse.BooleanOptionalAction, required=False, default=False)
+
+
     args = parser.parse_args()
 
     es_client = Elasticsearch(
@@ -50,6 +52,31 @@ def main():
         es_client.indices.put_settings(index=args.index_name, settings={'index.number_of_replicas': 0})
         print(f"Index '{args.index_name}' created with the specified schema.")
 
+    #Create inference endpoints for e5, elser and elser if they do not exist
+    if args.create_inference_endpoints:
+        print("Checking inference endpoints (ELSER)...")
+        try:
+            es_client.inference.get(inference_id="elser")
+        except NotFoundError as e:
+            print("Creating ELSER inference endpoint ...")
+            inference_config = json.load(open(os.path.join(args.config_folder, "inference_elser.json"), 'r'))
+            es_client.inference.put(inference_id="elser", inference_config=inference_config)
+
+        print("Checking inference endpoints (e5)...")
+        try:
+            es_client.inference.get(inference_id="e5")
+        except NotFoundError as e:
+            print("Creating e5 inference endpoint ...")
+            inference_config = json.load(open(os.path.join(args.config_folder, "inference_e5.json"), 'r'))
+            es_client.inference.put(inference_id="e5", inference_config=inference_config)
+
+        print("Checking inference endpoints (rerank)...")
+        try:
+            es_client.inference.get(inference_id="rerank")
+        except NotFoundError as e:
+            print("Creating rerank inference endpoint ...")
+            inference_config = json.load(open(os.path.join(args.config_folder, "inference_rerank.json"), 'r'))
+            es_client.inference.put(inference_id="rerank", inference_config=inference_config)
 
     print("Indexing data into Elasticsearch...")
     movies_file_path = os.path.join(args.data_folder, "movies.json.gz")
